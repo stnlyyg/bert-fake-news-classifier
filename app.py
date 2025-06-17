09d300aa42478a4e23d08ef178e4f56eca400464
stanleyyg11 <stanleyyong8888@gmail.com>
2025-06-17 19:57:26 +0700

    Added evaluation with confusion matrix. Created inference function for gradio interface

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
 # Create simple Gradio app → Load model → Predict news text input
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+import gradio as gr
 
 model_path = './model/fake-news-detection/checkpoint-10731'
 
-app_model = AutoModelForSequenceClassification.from_pretrained(model_path)
-app_tokenizer = AutoTokenizer.from_pretrained(model_path)
+finetuned_model = AutoModelForSequenceClassification.from_pretrained(model_path)
+finetuned_tokenizer = AutoTokenizer.from_pretrained(model_path)
+
+labels = ['Real', 'Fake']
+
+def classify_news(text):
+    inputs = finetuned_tokenizer(text, return_tensors='pt', truncation=True, padding=True, max_length=256)
+
+    with torch.no_grad():
+        outputs = finetuned_model(**inputs)
+
+    probs = torch.nn.functional.softmax(outputs.logits, dim=-1)
+    label_index = torch.argmax(probs).item()
+    confidence = probs[0][label_index]
+
+    predicted_label = labels[label_index]
+
+    return f'This news is: {predicted_label}, confidence: {confidence}'
+
+if __name__ == '__main__':
+    news = input("Enter news: ")
+    print(classify_news(news))
--- evaluation.py
+++ evaluation.py
@@ -1,1 +1,44 @@
 # Load trained model → Predict on test set → Evaluate (confusion matrix, F1, etc)
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer
+from sklearn.metrics import classification_report, confusion_matrix
+from dataset import eval_dataset
+
+model_path = "model/fake-news-detection/checkpoint-10731"
+
+def evaluate_model():
+    bert_model = AutoModelForSequenceClassification.from_pretrained(model_path)
+
+    eval_args = TrainingArguments(
+        output_dir = "./eval_results",
+        per_device_eval_batch_size = 8,
+        do_train = False,
+        do_eval = True
+    )
+
+    trainer = Trainer(
+        model = bert_model,
+        args = eval_args
+    )
+
+    predictions = trainer.predict(eval_dataset) #output of this code are PredictionOutput object containing predictions, label_ids, and metrics
+    y_true = predictions.label_ids
+    y_pred = np.argmax(predictions.predictions, axis=-1)
+    target_names = ["Real", "Fake"]
+    eval_report = classification_report(y_true, y_pred, target_names=target_names, digits=4)
+
+    print(f'Classification evaluation report: \n{eval_report}')
+
+    conf_matrix = confusion_matrix(y_true, y_pred)
+    plt.figure(figsize=(6, 4))
+    sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Greens', xticklabels=target_names, yticklabels=target_names)
+    plt.xlabel('Predicted Label')
+    plt.ylabel('True Label')
+    plt.title('Confusion Matrix')
+
+    plt.show()
+
+if __name__ == '__main__':
+    evaluate_model()
